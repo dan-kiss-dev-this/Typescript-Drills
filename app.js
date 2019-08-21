be9016c832a11e0ff2a0a13e8b6df77ac497e2f1
Dan Kiss <flowfast47@gmail.com>
2019-08-21 11:00:08 -0700

    add in enum iteration

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 console.log('Hello ExecuteAutomation');
+var a = true;
+var b = 'Karthik';
+var k = 1234;
+console.log('a :', a);
+var LanguagesKnown;
+(function (LanguagesKnown) {
+    LanguagesKnown[LanguagesKnown["English"] = 0] = "English";
+    LanguagesKnown[LanguagesKnown["Hindi"] = 1] = "Hindi";
+    LanguagesKnown[LanguagesKnown["Tamil"] = 2] = "Tamil";
+})(LanguagesKnown || (LanguagesKnown = {}));
+var studentsList = [
+    { Name: "Prashanth", Age: 26, Phone: 234 - 234 - 2341, Language: LanguagesKnown.English },
+    { Name: "Roberto", Age: 25, Phone: 234 - 234 - 2342, Language: LanguagesKnown.Hindi }
+];
+var newStudent = { Name: "Reddy", Age: 24, Phone: 234 - 234 - 2343, Language: LanguagesKnown.Tamil };
+studentsList.push(newStudent);
+for (var index = 0; index < studentsList.length; index++) {
+    var element = studentsList[index];
+    console.log(element);
+}
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,5 +1,26 @@
 
-
 console.log('Hello ExecuteAutomation');
 
+let a: boolean = true;
 
+let b: string = 'Karthik';
+
+let k: number = 1234;
+
+console.log('a :', a);
+
+enum LanguagesKnown { English, Hindi, Tamil }
+
+let studentsList = [
+  { Name: "Prashanth", Age: 26, Phone: 234 - 234 - 2341, Language: LanguagesKnown.English },
+  { Name: "Roberto", Age: 25, Phone: 234 - 234 - 2342, Language: LanguagesKnown.Hindi }
+]
+
+let newStudent = { Name: "Reddy", Age: 24, Phone: 234 - 234 - 2343, Language: LanguagesKnown.Tamil };
+
+studentsList.push(newStudent);
+
+for (let index = 0; index < studentsList.length; index++) {
+  const element = studentsList[index];
+  console.log(element)
+}
